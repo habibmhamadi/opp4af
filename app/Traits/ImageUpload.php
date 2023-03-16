a28411e8aaa9dfeb6296ef1a2cfbce4f3e00d3bf
Habibullah Mohammadi <34987353+habibmhamadi@users.noreply.github.com>
2023-03-16 10:03:54 +0430

    Update ImageUpload.php

--- app/Traits/ImageUpload.php
+++ app/Traits/ImageUpload.php
@@ -2,19 +2,28 @@
 
 namespace App\Traits;
 
+use Illuminate\Support\Facades\Storage;
+
 trait ImageUpload {
     protected function uploadImage($image)
     {
         $imageName = time().'.'.$image->extension();
-        $image->move(public_path('images'), $imageName);
+//         $image->move(public_path('images'), $imageName);
+        $image->move(storage_path('app/public/images'), $imageName);
         return $imageName;
     }
 
     protected function deleteImage($image)
     {
-        if(! preg_match('/http.*/', $image) && file_exists($image))
+//         if(! preg_match('/http.*/', $image) && file_exists($image))
+//         {
+//             unlink($image);   
+//         }
+        if(!$image) return;
+        
+        if(! preg_match('/http.*/', $image) && Storage::exists('public/images/'.$image))
         {
-            unlink($image);   
+            Storage::delete('public/images/'.$image); 
         }
     }
 }
